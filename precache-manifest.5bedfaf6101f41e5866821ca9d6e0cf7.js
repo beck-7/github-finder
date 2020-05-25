21cc896097066f13ba09dfb3351bf9be6ddd53bb
Beck <beckdev7@gmail.com>
2020-05-25 23:50:29 +0600

    Updates

---
+++ precache-manifest.5bedfaf6101f41e5866821ca9d6e0cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a5fb23919d9ecb212d6b9f71991dfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "042b72683e18f19eb6be",
+    "url": "/static/css/main.06118106.chunk.css"
+  },
+  {
+    "revision": "bbf606cc59d347472674",
+    "url": "/static/js/2.1beda760.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1beda760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042b72683e18f19eb6be",
+    "url": "/static/js/main.b9259b81.chunk.js"
+  },
+  {
+    "revision": "f7405e0a063efadb3e67",
+    "url": "/static/js/runtime-main.f431ffee.js"
+  },
+  {
+    "revision": "a6949c0b763232615740673328211f0d",
+    "url": "/static/media/spinner.a6949c0b.gif"
+  }
+]);
